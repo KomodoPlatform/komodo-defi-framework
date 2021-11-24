3525bd9d081568382abbb1af085d747b702fdf27
milerius <rmscastle@gmail.com>
2021-11-24 10:16:59 +0100

    feat(fix): remove usage of block_on in spawned function

--- mm2src/mm2_lib/mm2_native_lib.rs
+++ mm2src/mm2_lib/mm2_native_lib.rs
@@ -241,7 +241,7 @@ pub extern "C" fn mm2_stop() -> i8 {
     }
 
     spawn(async move {
-        block_on(dispatch_lp_event(ctx.clone(), StopCtxEvent {}.into()));
+        dispatch_lp_event(ctx.clone(), StopCtxEvent {}.into()).await;
         let _ = ctx.stop();
     });
 
