d15210acbfde1378d257060c0e6869cb8cb288a6
borngraced <51881311+borngraced@users.noreply.github.com>
2022-05-26 01:01:57 +0100

    fix wasm

--- mm2src/mm2_main/src/lp_price.rs
+++ mm2src/mm2_main/src/lp_price.rs
@@ -3,7 +3,6 @@ use common::StatusCode;
 use common::{log::{debug, error},
              mm_number::BigDecimal};
 use mm2_err_handle::prelude::{MmError, OrMmError};
-use mm2_net::native_http::slurp_url;
 use mm2_net::transport::SlurpError;
 use std::collections::HashMap;
 use std::str::Utf8Error;
@@ -170,9 +169,22 @@ impl TickerInfosRegistry {
     }
 }
 
+#[cfg(not(target_arch = "wasm32"))]
 pub async fn process_price_request(price_url: &str) -> Result<TickerInfosRegistry, MmError<PriceServiceRequestError>> {
     debug!("Fetching price from: {}", price_url);
-    let (status, headers, body) = slurp_url(price_url).await?;
+    let (status, headers, body) = mm2_net::native_http::slurp_url(price_url).await?;
+    let (status_code, body, _) = (status, std::str::from_utf8(&body)?.trim().into(), headers);
+    if status_code != StatusCode::OK {
+        return MmError::err(PriceServiceRequestError::HttpProcessError(body));
+    }
+    let model: HashMap<String, TickerInfos> = serde_json::from_str(&body)?;
+    Ok(TickerInfosRegistry(model))
+}
+
+#[cfg(target_arch = "wasm32")]
+pub async fn process_price_request(price_url: &str) -> Result<TickerInfosRegistry, MmError<PriceServiceRequestError>> {
+    debug!("Fetching price from: {}", price_url);
+    let (status, headers, body) = mm2_net::wasm_http::slurp_url(price_url).await?;
     let (status_code, body, _) = (status, std::str::from_utf8(&body)?.trim().into(), headers);
     if status_code != StatusCode::OK {
         return MmError::err(PriceServiceRequestError::HttpProcessError(body));
