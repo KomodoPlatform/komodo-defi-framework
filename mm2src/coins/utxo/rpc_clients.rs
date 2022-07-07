11e2cb7bbbe00a1726ffc5ab05ff751252831525
shamardy <shamardy@yahoo.com>
2022-07-07 20:02:30 +0200

    continued: btc difficulty calculations for spv validation wip, added more test cases

--- mm2src/coins/utxo/rpc_clients.rs
+++ mm2src/coins/utxo/rpc_clients.rs
@@ -2799,15 +2799,16 @@ pub fn retarget_timespan(retarget_timestamp: u32, last_timestamp: u32) -> u32 {
     range_constrain(timespan, MIN_TIMESPAN as i64, MAX_TIMESPAN as i64) as u32
 }
 
-// height can be replaced with last_header.height + 1
 pub async fn btc_mainnet_next_block_bits(
-    last_header: BlockHeader,
-    height: u32,
-    store: BlockHeaderStorage,
+    last_block_header: BlockHeader,
+    last_block_height: u32,
+    store: &BlockHeaderStorage,
 ) -> Result<BlockHeaderBits, NextBlockBitsError> {
-    if height == 0 {
-        return Err(NextBlockBitsError::Internal("Next block height can't be zero".into()));
+    if last_block_height == 0 {
+        return Err(NextBlockBitsError::Internal("Last block height can't be zero".into()));
     }
+
+    let height = last_block_height + 1;
 
     if height % RETARGETING_INTERVAL == 0 {
         let retarget_ref = (height - RETARGETING_INTERVAL).into();
@@ -2815,15 +2816,16 @@ pub async fn btc_mainnet_next_block_bits(
         // timestamp of block(height - RETARGETING_INTERVAL)
         let retarget_timestamp = retarget_header.time;
         // timestamp of current block
-        let last_timestamp = last_header.time;
+        let last_timestamp = last_block_header.time;
 
-        let retarget: Compact = last_header.bits.into();
+        let retarget: Compact = last_block_header.bits.into();
         let retarget: U256 = retarget.into();
         let retarget_timespan: U256 = retarget_timespan(retarget_timestamp, last_timestamp).into();
         let retarget: U256 = retarget * retarget_timespan;
         let target_timespan_seconds: U256 = TARGET_TIMESPAN_SECONDS.into();
         let retarget = retarget / target_timespan_seconds;
 
+        // todo: use a const
         let maximum: U256 = "00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
             .parse()
             .expect("hardcoded value should parse without errors");
@@ -2834,7 +2836,7 @@ pub async fn btc_mainnet_next_block_bits(
             Ok(BlockHeaderBits::Compact(retarget.into()))
         }
     } else {
-        Ok(last_header.bits)
+        Ok(last_block_header.bits)
     }
 }
 
@@ -2873,8 +2875,31 @@ mod tests {
 
         let last_header: BlockHeader = "000000201d758432ecd495a2177b44d3fe6c22af183461a0b9ea0d0000000000000000008283a1dfa795d9b68bd8c18601e443368265072cbf8c76bfe58de46edd303798035de95d3eb2151756fdb0e8".into();
 
-        let btc_mainnet_next_block_bits = block_on(btc_mainnet_next_block_bits(last_header, 606816, storage)).unwrap();
+        let next_block_bits = block_on(btc_mainnet_next_block_bits(last_header, 606815, &storage)).unwrap();
 
-        assert_eq!(btc_mainnet_next_block_bits, BlockHeaderBits::Compact(387308498.into()))
+        assert_eq!(next_block_bits, BlockHeaderBits::Compact(387308498.into()));
+
+        // check that bits for very early blocks that didn't change difficulty because of low hashrate is calculated correctly.
+        // https://live.blockcypher.com/btc/block/00000000a141216a896c54f211301c436e557a8d55900637bbdce14c6c7bddef/
+        let block_header = ElectrumBlockHeaderV14 {
+            height: 2016,
+            hex: "010000006397bb6abd4fc521c0d3f6071b5650389f0b4551bc40b4e6b067306900000000ace470aecda9c8818c8fe57688cd2a772b5a57954a00df0420a7dd546b6d2c576b0e7f49ffff001d33f0192f".into(),
+        }.into();
+        let headers = vec![ElectrumBlockHeader::V14(block_header)];
+        block_on(storage.add_electrum_block_headers_to_storage(for_coin, headers)).unwrap();
+
+        let last_header: BlockHeader = "010000000d9c8c96715756b619116cc2160937fb26c655a2f8e28e3a0aff59c0000000007676252e8434de408ea31920d986aba297bd6f7c6f20756be08748713f7c135962719449ffff001df8c1cb01".into();
+
+        let next_block_bits = block_on(btc_mainnet_next_block_bits(last_header, 4031, &storage)).unwrap();
+
+        assert_eq!(next_block_bits, BlockHeaderBits::Compact(486604799.into()));
+
+        // check that bits stay the same when the next block is not a retarget block
+        // https://live.blockcypher.com/btc/block/00000000000000000002622f52b6afe70a5bb139c788e67f221ffc67a762a1e0/
+        let last_header: BlockHeader = "00e0ff2f44d953fe12a047129bbc7164668c6d96f3e7a553528b02000000000000000000d0b950384cd23ab0854d1c8f23fa7a97411a6ffd92347c0a3aea4466621e4093ec09c762afa7091705dad220".into();
+
+        let next_block_bits = block_on(btc_mainnet_next_block_bits(last_header, 744014, &storage)).unwrap();
+
+        assert_eq!(next_block_bits, BlockHeaderBits::Compact(386508719.into()));
     }
 }
