be591297f39950347f870289067c6b5577da656f
Onur Özkan <work@onurozkan.dev>
2022-07-28 14:06:02 +0300

    update `get_public_address` to uncompress format for ethereum
    Signed-off-by: Onur Özkan <work@onurozkan.dev>

--- mm2src/coins/eth.rs
+++ mm2src/coins/eth.rs
@@ -1164,11 +1164,8 @@ impl MarketCoinOps for EthCoin {
     fn my_address(&self) -> Result<String, String> { Ok(checksum_address(&format!("{:#02x}", self.my_address))) }
 
     fn get_public_key(&self) -> Result<String, MmError<UnexpectedDerivationMethod>> {
-        let mut raw_pubkey = [0; 65];
-        raw_pubkey[0] = 0x04;
-        raw_pubkey[1..].copy_from_slice(self.key_pair.public());
-        let secp_public = PublicKey::from_slice(&raw_pubkey).unwrap();
-        Ok(secp_public.to_string())
+        let uncompressed_without_prefix = hex::encode(self.key_pair.public());
+        Ok(format!("04{}", uncompressed_without_prefix))
     }
 
     /// Hash message for signature using Ethereum's message signing format.
