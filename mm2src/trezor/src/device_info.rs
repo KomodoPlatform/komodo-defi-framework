4d922395f6e9e88681020d189aded161ae1d9c0b
Sergey Boyko <sergey.boyko0791@gmail.com>
2022-08-11 15:50:36 +0200

    Remove `TrezorModel` enum

--- mm2src/trezor/src/device_info.rs
+++ mm2src/trezor/src/device_info.rs
@@ -1,26 +1,9 @@
 use crate::proto::messages_management::Features;
 
-#[derive(Clone, Debug, Serialize)]
-pub enum TrezorModel {
-    One,
-    T,
-    Other(String),
-}
-
-impl From<String> for TrezorModel {
-    fn from(model: String) -> Self {
-        match model.as_str() {
-            "1" => TrezorModel::One,
-            "T" => TrezorModel::T,
-            _ => TrezorModel::Other(model),
-        }
-    }
-}
-
 #[derive(Debug, Clone, Serialize)]
 pub struct TrezorDeviceInfo {
     /// The device model.
-    model: Option<TrezorModel>,
+    model: Option<String>,
     /// Name given to the device.
     device_name: Option<String>,
     /// Unique device identifier.
@@ -30,7 +13,7 @@ pub struct TrezorDeviceInfo {
 impl From<Features> for TrezorDeviceInfo {
     fn from(features: Features) -> Self {
         TrezorDeviceInfo {
-            model: features.model.map(TrezorModel::from),
+            model: features.model,
             device_name: features.label,
             device_id: features.device_id,
         }
