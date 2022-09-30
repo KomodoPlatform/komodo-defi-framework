d762c66ec3153218a3de925e0096de45aab015a9
Artem Vitae <email@not.set>
2022-09-30 13:49:14 +0300

    Fix some tests.

--- mm2src/mm2_main/src/mm2_tests/tendermint_tests.rs
+++ mm2src/mm2_main/src/mm2_tests/tendermint_tests.rs
@@ -54,7 +54,7 @@ fn test_tendermint_withdraw() {
     ));
     println!("Withdraw to other {}", json::to_string(&withdraw_result).unwrap());
     let tx_details: TransactionDetails = json::from_value(withdraw_result).unwrap();
-    let expected_total: BigDecimal = "0.101".parse().unwrap();
+    let expected_total: BigDecimal = "0.15".parse().unwrap();
     assert_eq!(tx_details.total_amount, expected_total);
     assert_eq!(tx_details.spent_by_me, expected_total);
     assert_eq!(tx_details.my_balance_change, expected_total * BigDecimal::from(-1));
@@ -76,9 +76,9 @@ fn test_tendermint_withdraw() {
     println!("Withdraw to self {}", json::to_string(&withdraw_result).unwrap());
 
     let tx_details: TransactionDetails = json::from_value(withdraw_result).unwrap();
-    let expected_total: BigDecimal = "0.101".parse().unwrap();
+    let expected_total: BigDecimal = "0.15".parse().unwrap();
     let expected_received: BigDecimal = "0.1".parse().unwrap();
-    let expected_balance_change: BigDecimal = "-0.001".parse().unwrap();
+    let expected_balance_change: BigDecimal = "-0.05".parse().unwrap();
     assert_eq!(tx_details.total_amount, expected_total);
     assert_eq!(tx_details.spent_by_me, expected_total);
     assert_eq!(tx_details.my_balance_change, expected_balance_change);
@@ -126,7 +126,7 @@ fn test_tendermint_token_activation_and_withdraw() {
     let expected_total: BigDecimal = "0.1".parse().unwrap();
     assert_eq!(tx_details.total_amount, expected_total);
 
-    let expected_fee: BigDecimal = "0.001".parse().unwrap();
+    let expected_fee: BigDecimal = "0.05".parse().unwrap();
     let actual_fee: BigDecimal = tx_details.fee_details["amount"].as_str().unwrap().parse().unwrap();
     assert_eq!(actual_fee, expected_fee);
 
@@ -155,7 +155,7 @@ fn test_tendermint_token_activation_and_withdraw() {
 
     assert_eq!(tx_details.total_amount, expected_total);
 
-    let expected_fee: BigDecimal = "0.001".parse().unwrap();
+    let expected_fee: BigDecimal = "0.05".parse().unwrap();
     let actual_fee: BigDecimal = tx_details.fee_details["amount"].as_str().unwrap().parse().unwrap();
     assert_eq!(actual_fee, expected_fee);
 
