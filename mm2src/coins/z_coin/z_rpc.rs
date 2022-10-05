09840b9ba185d97ee385ee29cff839d99d262c22
laruh <oblomoff616@gmail.com>
2022-10-05 18:59:46 +0700

    remove mutex

--- mm2src/coins/z_coin/z_rpc.rs
+++ mm2src/coins/z_coin/z_rpc.rs
@@ -16,7 +16,6 @@ use mm2_err_handle::prelude::*;
 use parking_lot::Mutex;
 use prost::Message;
 use protobuf::Message as ProtobufMessage;
-use std::ops::Deref;
 use std::path::{Path, PathBuf};
 use std::str::FromStr;
 use std::sync::Arc;
@@ -51,8 +50,7 @@ struct CompactBlockRow {
     data: Vec<u8>,
 }
 
-pub type OnCompactBlockFn<'a> =
-    dyn FnMut(TonicCompactBlock) -> Result<(), MmError<UpdateBlocksCacheErr>> + Send + Sync + 'a;
+pub type OnCompactBlockFn<'a> = dyn FnMut(TonicCompactBlock) -> Result<(), MmError<UpdateBlocksCacheErr>> + Send + 'a;
 
 #[async_trait]
 pub trait ZRpcOps {
@@ -379,7 +377,7 @@ pub(super) async fn init_light_client(
 
     let sync_handle = SaplingSyncLoopHandle {
         current_block: BlockHeight::from_u32(0),
-        blocks_db: Mutex::new(blocks_db),
+        blocks_db,
         wallet_db: wallet_db.clone(),
         consensus_params,
         sync_status_notifier,
@@ -413,7 +411,7 @@ pub(super) async fn init_native_client(
 
     let sync_handle = SaplingSyncLoopHandle {
         current_block: BlockHeight::from_u32(0),
-        blocks_db: Mutex::new(blocks_db),
+        blocks_db,
         wallet_db: wallet_db.clone(),
         consensus_params,
         sync_status_notifier,
@@ -479,7 +477,7 @@ pub enum SyncStatus {
 
 pub struct SaplingSyncLoopHandle {
     current_block: BlockHeight,
-    blocks_db: Mutex<BlockDb>,
+    blocks_db: BlockDb,
     wallet_db: WalletDbShared,
     consensus_params: ZcoinConsensusParams,
     /// Notifies about sync status without stopping the loop, e.g. on coin activation
@@ -528,7 +526,7 @@ impl SaplingSyncLoopHandle {
         rpc: &mut (dyn ZRpcOps + Send),
     ) -> Result<(), MmError<UpdateBlocksCacheErr>> {
         let current_block = rpc.get_block_height().await?;
-        let current_block_in_db = block_in_place(|| self.blocks_db.lock().get_latest_block())?;
+        let current_block_in_db = block_in_place(|| self.blocks_db.get_latest_block())?;
         let extrema = block_in_place(|| self.wallet_db.lock().block_height_extrema())?;
         let mut from_block = self
             .consensus_params
@@ -540,11 +538,7 @@ impl SaplingSyncLoopHandle {
         }
         if current_block >= from_block {
             rpc.scan_blocks(from_block, current_block, &mut |block: TonicCompactBlock| {
-                block_in_place(|| {
-                    self.blocks_db
-                        .lock()
-                        .insert_block(block.height as u32, block.encode_to_vec())
-                })?;
+                block_in_place(|| self.blocks_db.insert_block(block.height as u32, block.encode_to_vec()))?;
                 self.notify_blocks_cache_status(block.height, current_block);
                 Ok(())
             })
@@ -564,7 +558,7 @@ impl SaplingSyncLoopHandle {
 
         if let Err(e) = validate_chain(
             &self.consensus_params,
-            self.blocks_db.lock().deref(),
+            &self.blocks_db,
             wallet_ops.get_max_height_hash()?,
         ) {
             match e {
@@ -575,13 +569,13 @@ impl SaplingSyncLoopHandle {
                         BlockHeight::from_u32(0)
                     };
                     wallet_ops.rewind_to_height(rewind_height)?;
-                    self.blocks_db.lock().rewind_to_height(rewind_height.into())?;
+                    self.blocks_db.rewind_to_height(rewind_height.into())?;
                 },
                 e => return MmError::err(e),
             }
         }
 
-        let current_block = BlockHeight::from_u32(self.blocks_db.lock().get_latest_block()?);
+        let current_block = BlockHeight::from_u32(self.blocks_db.get_latest_block()?);
         loop {
             match wallet_ops.block_height_extrema()? {
                 Some((_, max_in_wallet)) => {
@@ -593,12 +587,7 @@ impl SaplingSyncLoopHandle {
                 },
                 None => self.notify_building_wallet_db(0, current_block.into()),
             }
-            scan_cached_blocks(
-                &self.consensus_params,
-                self.blocks_db.lock().deref(),
-                &mut wallet_ops,
-                Some(1000),
-            )?;
+            scan_cached_blocks(&self.consensus_params, &self.blocks_db, &mut wallet_ops, Some(1000))?;
         }
         Ok(())
     }
