e48799cfa8e40b12e3cccad33f2db0ded128e974
borngraced <samuelonoja970@gmail.com>
2022-12-12 15:46:29 +0100

    minor fixes
    Signed-off-by: borngraced <samiodev@icloud.com>

--- mm2src/derives/enum_from/src/from_stringify.rs
+++ mm2src/derives/enum_from/src/from_stringify.rs
@@ -1,9 +1,10 @@
 use crate::{CompileError, IdentCtx};
-use proc_macro2::Ident;
+use proc_macro2::{Ident, TokenStream};
 use quote::__private::ext::RepToTokensExt;
-use quote::{quote, quote_spanned, ToTokens};
+use quote::{quote, quote_spanned};
 use syn::__private::TokenStream2;
 use syn::punctuated::Punctuated;
+use syn::spanned::Spanned;
 use syn::token::Comma;
 use syn::Variant;
 
@@ -29,60 +30,51 @@ pub(crate) enum InnerIdentTypes {
 }
 
 pub(crate) fn get_inner_ident_type(ident: Option<Ident>) -> InnerIdentTypes {
-    if let Some(ident) = ident {
-        return if ident == Ident::new("String", ident.span()) {
-            InnerIdentTypes::String
-        } else {
-            InnerIdentTypes::Named
-        };
-    }
-    InnerIdentTypes::None
+    ident
+        .map(|ident| {
+            if ident == Ident::new("String", ident.span()) {
+                InnerIdentTypes::String
+            } else {
+                InnerIdentTypes::Named
+            }
+        })
+        .unwrap_or(InnerIdentTypes::None)
 }
 
-pub(crate) fn get_attributes(variants: Variant) -> Result<MapEnumDataPunctuated, syn::Error> {
+pub(crate) fn get_attributes(variants: Variant) -> Result<MapEnumDataPunctuated, TokenStream> {
     let variant_ident = &variants.ident;
     let fields = &variants.fields;
+
     for attribute in variants.attrs {
         if let Ok(meta) = attribute.parse_meta() {
             return match meta {
                 syn::Meta::List(syn::MetaList { nested, .. }) => {
-                    if let Some(ident) = get_variant_unnamed_ident(fields.to_owned()) {
-                        return Ok(MapEnumDataPunctuated {
-                            variant_ident: variant_ident.to_owned(),
-                            nested_meta: nested,
-                            inner_ident: Some(ident),
-                        });
-                    }
-                    Ok(MapEnumDataPunctuated {
+                    return Ok(MapEnumDataPunctuated {
                         variant_ident: variant_ident.to_owned(),
                         nested_meta: nested,
-                        inner_ident: None,
-                    })
+                        inner_ident: get_variant_unnamed_ident(fields.to_owned()),
+                    });
                 },
-                _ => Err(syn::Error::new_spanned(
-                    attribute.tokens,
-                    "expected #[enum_from_variant(..)]".to_string(),
+                _ => Err(quote_spanned!(
+                attribute.tokens.span() => compile_error!("expected #[enum_from_stringify(..)]")
                 )),
             };
         };
     }
-    Err(syn::Error::new_spanned(
-        variant_ident.to_token_stream(),
-        "Operation Error.".to_string(),
+    Err(quote_spanned!(
+    variant_ident.span() => compile_error!("expected #[enum_from_stringify(..)]")
     ))
 }
 
 fn get_variant_unnamed_ident(fields: syn::Fields) -> Option<Ident> {
-    if let syn::Fields::Unnamed(fields_unnamed) = fields {
-        let syn::FieldsUnnamed { unnamed, .. } = fields_unnamed;
+    if let syn::Fields::Unnamed(syn::FieldsUnnamed { unnamed, .. }) = fields {
         if let Some(field) = unnamed.iter().next() {
             let type_path = if let Some(syn::Type::Path(type_path, ..)) = field.ty.next().cloned() {
                 type_path
             } else {
                 return None;
             };
-            let path_segment = type_path.path.segments.iter().next().cloned()?;
-            return Some(path_segment.ident);
+            return Some(type_path.path.segments.iter().next().cloned()?.ident);
         };
     }
     None
@@ -114,7 +106,8 @@ pub(crate) fn impl_from_stringify(ctx: &IdentCtx<'_>, variant: &Variant) -> Resu
                 str.span() => compile_error!("Expected this to be an `Ident`")
                 ));
             };
-            let ident_to_impl_from = syn::Ident::new(&str.value(), str.span());
+
+            let ident_to_impl_from = Ident::new(&str.value(), str.span());
             return match get_inner_ident_type(m.inner_ident.to_owned()) {
                 InnerIdentTypes::Named => Some(quote! {
                     impl From<#ident_to_impl_from> for #enum_name {
