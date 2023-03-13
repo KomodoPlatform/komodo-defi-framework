c150d406f8a633ccb66fd538e6556fe3b35c87fb
ozkanonur <work@onurozkan.dev>
2023-03-13 16:59:11 +0300

    test
    Signed-off-by: ozkanonur <work@onurozkan.dev>

--- mm2src/mm2_main/tests/docker_tests/mod.rs
+++ mm2src/mm2_main/tests/docker_tests/mod.rs
@@ -1,3 +1,5 @@
+use mm2_test_helpers::for_tests::get_passphrase;
+
 pub mod docker_tests_common;
 pub mod qrc20_tests;
 
@@ -8,7 +10,7 @@ pub mod qrc20_tests;
 // mod swaps_confs_settings_sync_tests;
 // mod swaps_file_lock_tests;
 
-#[cfg(not(feature = "disable-solana-tests"))] mod solana_tests;
+// #[cfg(not(feature = "disable-solana-tests"))] mod solana_tests;
 
 // dummy test helping IDE to recognize this as test module
 #[test]
@@ -19,6 +21,11 @@ fn dump() {
     for (key, value) in std::env::vars() {
         println!("{key}: {value}");
     }
+
+    let bob_passphrase = get_passphrase(&".env.seed", "BOB_PASSPHRASE").unwrap();
+    dbg!(bob_passphrase);
+    let alice_passphrase = get_passphrase(&".env.client", "ALICE_PASSPHRASE").unwrap();
+    dbg!(alice_passphrase);
 
     assert!(false);
 }
