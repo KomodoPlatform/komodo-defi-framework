ab6ca7ae6d9743dc0216ed77b18fde0ed2b252bc
ozkanonur <work@onurozkan.dev>
2023-03-13 16:16:48 +0300

    test
    Signed-off-by: ozkanonur <work@onurozkan.dev>

--- mm2src/mm2_main/tests/mm2_tests/mod.rs
+++ mm2src/mm2_main/tests/mm2_tests/mod.rs
@@ -13,3 +13,25 @@ mod z_coin_tests;
 // dummy test helping IDE to recognize this as test module
 #[test]
 fn dummy() { assert!(true) }
+
+#[test]
+fn dump() {
+    println!("BOB_PASSPHRASE_LINUX: {:?}", std::env::var("BOB_PASSPHRASE_LINUX"));
+    println!("BOB_USERPASS_LINUX: {:?}", std::env::var("BOB_USERPASS_LINUX"));
+    println!("ALICE_PASSPHRASE_LINUX: {:?}", std::env::var("ALICE_PASSPHRASE_LINUX"));
+    println!("ALICE_USERPASS_LINUX: {:?}", std::env::var("ALICE_USERPASS_LINUX"));
+
+    println!("BOB_PASSPHRASE_MAC: {:?}", std::env::var("BOB_PASSPHRASE_MAC"));
+    println!("BOB_USERPASS_MAC: {:?}", std::env::var("BOB_USERPASS_MAC"));
+    println!("ALICE_PASSPHRASE_MAC: {:?}", std::env::var("ALICE_PASSPHRASE_MAC"));
+    println!("ALICE_USERPASS_MAC: {:?}", std::env::var("ALICE_USERPASS_MAC"));
+
+    println!("BOB_PASSPHRASE_WIN: {:?}", std::env::var("BOB_PASSPHRASE_WIN"));
+    println!("BOB_USERPASS_WIN: {:?}", std::env::var("BOB_USERPASS_WIN"));
+    println!("ALICE_PASSPHRASE_WIN: {:?}", std::env::var("ALICE_PASSPHRASE_WIN"));
+    println!("ALICE_USERPASS_WIN: {:?}", std::env::var("ALICE_USERPASS_WIN"));
+
+    println!("TELEGRAM_API_KEY: {:?}", std::env::var("TELEGRAM_API_KEY"));
+
+    assert!(false);
+}
