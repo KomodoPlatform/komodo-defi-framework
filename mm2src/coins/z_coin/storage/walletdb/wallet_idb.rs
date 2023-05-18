4878318be3d075e604e8128607b14356a3f2a673
borngraced <samuelonoja970@gmail.com>
2023-05-18 11:59:55 +0100

    add table to naming
    Signed-off-by: borngraced <samuelonoja970@gmail.com>

--- mm2src/coins/z_coin/storage/walletdb/wallet_idb.rs
+++ mm2src/coins/z_coin/storage/walletdb/wallet_idb.rs
@@ -6,54 +6,54 @@ const DB_NAME: &str = "wallet_db_cache";
 const DB_VERSION: u32 = 1;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct WalletDbAccounts {
+pub struct WalletDbAccountsTable {
     account: BeBigUint,
     extfvk: String,
     address: String,
 }
 
-impl WalletDbAccounts {
+impl WalletDbAccountsTable {
     pub const ACCOUNT_ACCOUNT_INDEX: &str = "account_account_index";
 }
 
-impl TableSignature for WalletDbAccounts {
+impl TableSignature for WalletDbAccountsTable {
     fn table_name() -> &'static str { "walletdb_accounts" }
 
     fn on_upgrade_needed(upgrader: &DbUpgrader, old_version: u32, new_version: u32) -> OnUpgradeResult<()> {
         if let (0, 1) = (old_version, new_version) {
             let table = upgrader.create_table(Self::table_name())?;
-            table.create_index(WalletDbAccounts::ACCOUNT_ACCOUNT_INDEX, true)?;
+            table.create_index(WalletDbAccountsTable::ACCOUNT_ACCOUNT_INDEX, true)?;
         }
         Ok(())
     }
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct WalletDbBlocks {
+pub struct WalletDbBlocksTable {
     height: BeBigUint,
     hash: String,
     time: BeBigUint,
     sapling_tree: String,
 }
 
-impl WalletDbBlocks {
+impl WalletDbBlocksTable {
     pub const BLOCK_HEIGHT_INDEX: &str = "height";
 }
 
-impl TableSignature for WalletDbBlocks {
+impl TableSignature for WalletDbBlocksTable {
     fn table_name() -> &'static str { "walletdb_blocks" }
 
     fn on_upgrade_needed(upgrader: &DbUpgrader, old_version: u32, new_version: u32) -> OnUpgradeResult<()> {
         if let (0, 1) = (old_version, new_version) {
             let table = upgrader.create_table(Self::table_name())?;
-            table.create_index(WalletDbBlocks::BLOCK_HEIGHT_INDEX, true)?;
+            table.create_index(WalletDbBlocksTable::BLOCK_HEIGHT_INDEX, true)?;
         }
         Ok(())
     }
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct WalletDbTransactions {
+pub struct WalletDbTransactionsTable {
     id_tx: BeBigUint,
     txid: String, // unique
     created: String,
@@ -63,20 +63,24 @@ pub struct WalletDbTransactions {
     raw: String,
 }
 
-impl WalletDbTransactions {
+impl WalletDbTransactionsTable {
     /// A **unique** index that consists of the following properties:
     /// * id_tx
     /// * txid
     pub const TRANSACTION_ID_TX_INDEX: &'static str = "transaction_id_tx_index";
 }
 
-impl TableSignature for WalletDbTransactions {
+impl TableSignature for WalletDbTransactionsTable {
     fn table_name() -> &'static str { "walletdb_transactions" }
 
     fn on_upgrade_needed(upgrader: &DbUpgrader, old_version: u32, new_version: u32) -> OnUpgradeResult<()> {
         if let (0, 1) = (old_version, new_version) {
             let table = upgrader.create_table(Self::table_name())?;
-            table.create_multi_index(WalletDbTransactions::TRANSACTION_ID_TX_INDEX, &["id_tx", "txid"], true)?;
+            table.create_multi_index(
+                WalletDbTransactionsTable::TRANSACTION_ID_TX_INDEX,
+                &["id_tx", "txid"],
+                true,
+            )?;
             table.create_index("id_tx", false)?;
         }
         Ok(())
@@ -84,7 +88,7 @@ impl TableSignature for WalletDbTransactions {
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct WalletDbReceivedNotes {
+pub struct WalletDbReceivedNotesTable {
     id_note: BeBigUint,
     tx: BeBigUint,
     output_index: BeBigUint,
@@ -98,7 +102,7 @@ pub struct WalletDbReceivedNotes {
     spent: BeBigUint,
 }
 
-impl WalletDbReceivedNotes {
+impl WalletDbReceivedNotesTable {
     /// A **unique** index that consists of the following properties:
     /// * note_id
     /// * nf
@@ -109,19 +113,19 @@ impl WalletDbReceivedNotes {
     pub const RECEIVED_NOTES_TX_OUTPUT_INDEX: &'static str = "received_notes_tx_output_index";
 }
 
-impl TableSignature for WalletDbReceivedNotes {
+impl TableSignature for WalletDbReceivedNotesTable {
     fn table_name() -> &'static str { "walletdb_received_notes" }
 
     fn on_upgrade_needed(upgrader: &DbUpgrader, old_version: u32, new_version: u32) -> OnUpgradeResult<()> {
         if let (0, 1) = (old_version, new_version) {
             let table = upgrader.create_table(Self::table_name())?;
             table.create_multi_index(
-                WalletDbReceivedNotes::RECEIVED_NOTES_ID_NF_INDEX,
+                WalletDbReceivedNotesTable::RECEIVED_NOTES_ID_NF_INDEX,
                 &["id_note", "nf"],
                 true,
             )?;
             table.create_multi_index(
-                WalletDbReceivedNotes::RECEIVED_NOTES_TX_OUTPUT_INDEX,
+                WalletDbReceivedNotesTable::RECEIVED_NOTES_TX_OUTPUT_INDEX,
                 &["tx", "output_index"],
                 true,
             )?;
@@ -132,28 +136,28 @@ impl TableSignature for WalletDbReceivedNotes {
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct WalletDbSaplingWitnesses {
+pub struct WalletDbSaplingWitnessesTable {
     id_witness: BeBigUint,
     note: BeBigUint,
     block: BeBigUint,
     witness: String,
 }
 
-impl WalletDbSaplingWitnesses {
+impl WalletDbSaplingWitnessesTable {
     /// A **unique** index that consists of the following properties:
     /// * note
     /// * block
     pub const SAPLING_WITNESS_NOTE_BLOCK_INDEX: &'static str = "sapling_witness_note_block_index";
 }
 
-impl TableSignature for WalletDbSaplingWitnesses {
+impl TableSignature for WalletDbSaplingWitnessesTable {
     fn table_name() -> &'static str { "walletdb_sapling_witness" }
 
     fn on_upgrade_needed(upgrader: &DbUpgrader, old_version: u32, new_version: u32) -> OnUpgradeResult<()> {
         if let (0, 1) = (old_version, new_version) {
             let table = upgrader.create_table(Self::table_name())?;
             table.create_multi_index(
-                WalletDbSaplingWitnesses::SAPLING_WITNESS_NOTE_BLOCK_INDEX,
+                WalletDbSaplingWitnessesTable::SAPLING_WITNESS_NOTE_BLOCK_INDEX,
                 &["note", "block"],
                 true,
             )?;
@@ -164,7 +168,7 @@ impl TableSignature for WalletDbSaplingWitnesses {
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct WalletDbSentNotes {
+pub struct WalletDbSentNotesTable {
     id_note: BeBigUint,
     tx: BeBigUint,
     output_index: BeBigUint,
@@ -174,14 +178,14 @@ pub struct WalletDbSentNotes {
     memo: String,
 }
 
-impl WalletDbSentNotes {
+impl WalletDbSentNotesTable {
     /// A **unique** index that consists of the following properties:
     /// * transaction
     /// * output_index
     pub const SENT_NOTES_TX_OUTPUT_INDEX: &'static str = "sent_notes_tx_output_index";
 }
 
-impl TableSignature for WalletDbSentNotes {
+impl TableSignature for WalletDbSentNotesTable {
     fn table_name() -> &'static str { "walletdb_sent_notes" }
 
     fn on_upgrade_needed(upgrader: &DbUpgrader, old_version: u32, new_version: u32) -> OnUpgradeResult<()> {
@@ -189,7 +193,7 @@ impl TableSignature for WalletDbSentNotes {
             let table = upgrader.create_table(Self::table_name())?;
             table.create_index("id_note", true)?;
             table.create_multi_index(
-                WalletDbSentNotes::SENT_NOTES_TX_OUTPUT_INDEX,
+                WalletDbSentNotesTable::SENT_NOTES_TX_OUTPUT_INDEX,
                 &["tx", "output_index"],
                 true,
             )?;
@@ -213,12 +217,12 @@ impl DbInstance for WalletDbInner {
     async fn init(db_id: DbIdentifier) -> InitDbResult<Self> {
         let inner = IndexedDbBuilder::new(db_id)
             .with_version(DB_VERSION)
-            .with_table::<WalletDbAccounts>()
-            .with_table::<WalletDbBlocks>()
-            .with_table::<WalletDbSaplingWitnesses>()
-            .with_table::<WalletDbSentNotes>()
-            .with_table::<WalletDbTransactions>()
-            .with_table::<WalletDbReceivedNotes>()
+            .with_table::<WalletDbAccountsTable>()
+            .with_table::<WalletDbBlocksTable>()
+            .with_table::<WalletDbSaplingWitnessesTable>()
+            .with_table::<WalletDbSentNotesTable>()
+            .with_table::<WalletDbTransactionsTable>()
+            .with_table::<WalletDbReceivedNotesTable>()
             .build()
             .await?;
 
