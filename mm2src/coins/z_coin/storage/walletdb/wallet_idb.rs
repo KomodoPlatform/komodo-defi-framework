2906e804bd655206bbbf70478770844e44997d0a
borngraced <samuelonoja970@gmail.com>
2023-05-30 18:27:17 +0100

    fix lint
    Signed-off-by: borngraced <samuelonoja970@gmail.com>

--- mm2src/coins/z_coin/storage/walletdb/wallet_idb.rs
+++ mm2src/coins/z_coin/storage/walletdb/wallet_idb.rs
@@ -26,7 +26,7 @@ impl TableSignature for WalletDbAccountsTable {
     fn on_upgrade_needed(upgrader: &DbUpgrader, old_version: u32, new_version: u32) -> OnUpgradeResult<()> {
         if let (0, 1) = (old_version, new_version) {
             let table = upgrader.create_table(Self::table_name())?;
-            table.create_multi_index(Self::ACCOUNT_TICKER_INDEX, &["ticker", "account"], true)?;
+            table.create_multi_index(Self::TICKER_ACCOUNT_INDEX, &["ticker", "account"], true)?;
             table.create_index("ticker", false)?;
         }
         Ok(())
