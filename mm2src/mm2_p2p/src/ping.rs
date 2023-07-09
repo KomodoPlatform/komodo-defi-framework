b2427cc996555ab93bd95570ff165f8cba2330a7
ozkanonur <work@onurozkan.dev>
2023-07-09 18:15:32 +0300

    complete ping implementation
    Signed-off-by: ozkanonur <work@onurozkan.dev>

--- mm2src/mm2_p2p/src/ping.rs
+++ mm2src/mm2_p2p/src/ping.rs
@@ -1,48 +1,60 @@
 use libp2p::ping::{Behaviour, Config, Event};
-use libp2p::swarm::{NetworkBehaviour, PollParameters, ToSwarm};
+use libp2p::swarm::{CloseConnection, NetworkBehaviour, PollParameters, ToSwarm};
+use log::error;
 use std::{collections::VecDeque,
           task::{Context, Poll}};
 use void::Void;
 
 pub struct AdexPing {
     ping: Behaviour,
-    events: VecDeque<ToSwarm<Void, <Self as NetworkBehaviour>::ConnectionHandler>>,
+    events: VecDeque<ToSwarm<<Behaviour as NetworkBehaviour>::ToSwarm, Void>>,
 }
 
 impl NetworkBehaviour for AdexPing {
     type ConnectionHandler = <Behaviour as NetworkBehaviour>::ConnectionHandler;
-
-    type ToSwarm = Void;
+    type ToSwarm = <Behaviour as NetworkBehaviour>::ToSwarm;
 
     fn handle_established_inbound_connection(
         &mut self,
-        _connection_id: libp2p::swarm::ConnectionId,
+        connection_id: libp2p::swarm::ConnectionId,
         peer: libp2p::PeerId,
         local_addr: &libp2p::Multiaddr,
         remote_addr: &libp2p::Multiaddr,
     ) -> Result<libp2p::swarm::THandler<Self>, libp2p::swarm::ConnectionDenied> {
-        todo!()
+        self.ping
+            .handle_established_inbound_connection(connection_id, peer, local_addr, remote_addr)
     }
 
     fn handle_established_outbound_connection(
         &mut self,
-        _connection_id: libp2p::swarm::ConnectionId,
+        connection_id: libp2p::swarm::ConnectionId,
         peer: libp2p::PeerId,
         addr: &libp2p::Multiaddr,
         role_override: libp2p::core::Endpoint,
     ) -> Result<libp2p::swarm::THandler<Self>, libp2p::swarm::ConnectionDenied> {
-        todo!()
+        self.ping
+            .handle_established_outbound_connection(connection_id, peer, addr, role_override)
     }
 
-    fn on_swarm_event(&mut self, event: libp2p::swarm::FromSwarm<Self::ConnectionHandler>) { todo!() }
+    fn on_swarm_event(&mut self, event: libp2p::swarm::FromSwarm<Self::ConnectionHandler>) {
+        self.ping.on_swarm_event(event)
+    }
 
     fn on_connection_handler_event(
         &mut self,
-        _peer_id: libp2p::PeerId,
-        _connection_id: libp2p::swarm::ConnectionId,
-        _event: libp2p::swarm::THandlerOutEvent<Self>,
+        peer_id: libp2p::PeerId,
+        connection_id: libp2p::swarm::ConnectionId,
+        event: libp2p::swarm::THandlerOutEvent<Self>,
     ) {
-        todo!()
+        if let Err(e) = &event {
+            error!("Ping error {}. Disconnecting peer {}", e, peer_id);
+            self.events.push_back(ToSwarm::CloseConnection {
+                peer_id,
+                connection: CloseConnection::All,
+            });
+        }
+
+        self.ping.on_connection_handler_event(peer_id, connection_id, event)
     }
 
     fn poll(
@@ -50,12 +62,12 @@ impl NetworkBehaviour for AdexPing {
         cx: &mut std::task::Context<'_>,
         params: &mut impl PollParameters,
     ) -> std::task::Poll<ToSwarm<Self::ToSwarm, libp2p::swarm::THandlerInEvent<Self>>> {
-        todo!()
-    }
-}
+        if let Some(event) = self.events.pop_front() {
+            return Poll::Ready(event);
+        }
 
-impl From<Event> for AdexPing {
-    fn from(value: Event) -> Self { todo!() }
+        Poll::Pending
+    }
 }
 
 #[allow(clippy::new_without_default)]
@@ -65,17 +77,5 @@ impl AdexPing {
             ping: Behaviour::new(Config::new()),
             events: VecDeque::new(),
         }
-    }
-
-    fn poll_event(
-        &mut self,
-        _cx: &mut Context,
-        _params: &mut impl PollParameters,
-    ) -> Poll<ToSwarm<Void, <Self as NetworkBehaviour>::ConnectionHandler>> {
-        if let Some(event) = self.events.pop_front() {
-            return Poll::Ready(event);
-        }
-
-        Poll::Pending
     }
 }
