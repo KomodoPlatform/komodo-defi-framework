ec2dfb329a6852904f7885ec79b0fb0f47c61e91
laruh <oblomoff616@gmail.com>
2024-01-21 00:09:15 +0700

    doc comm for `non_fungible_tokens_infos` field

--- mm2src/coins/eth.rs
+++ mm2src/coins/eth.rs
@@ -456,6 +456,8 @@ pub struct EthCoinImpl {
     logs_block_range: u64,
     nonce_lock: Arc<AsyncMutex<()>>,
     erc20_tokens_infos: Arc<Mutex<HashMap<String, Erc20TokenInfo>>>,
+    /// Stores NFT information where each value is uniquely identified by a string key composed of
+    /// the token address and token ID, separated by a comma.
     pub(crate) non_fungible_tokens_infos: Arc<AsyncMutex<HashMap<String, NftInfo>>>,
     /// This spawner is used to spawn coin's related futures that should be aborted on coin deactivation
     /// and on [`MmArc::stop`].
