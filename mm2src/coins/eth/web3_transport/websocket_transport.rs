6973845bb9bb70a3746691b522e8f52ed6fe52ec
onur-ozkan <work@onurozkan.dev>
2024-01-25 20:11:50 +0300

    save dev state
    Signed-off-by: onur-ozkan <work@onurozkan.dev>

--- mm2src/coins/eth/web3_transport/websocket_transport.rs
+++ mm2src/coins/eth/web3_transport/websocket_transport.rs
@@ -1,11 +1,14 @@
 #![allow(unused)] // TODO: remove this
 
-use crate::eth::web3_transport::Web3SendOut;
 use crate::eth::RpcTransportEventHandlerShared;
+use crate::{eth::web3_transport::Web3SendOut, rpc_command::lightning::nodes};
+use common::log;
 use futures::lock::Mutex as AsyncMutex;
 use jsonrpc_core::Call;
+use mm2_net::transport::GuiAuthValidationGenerator;
 use std::sync::{atomic::{AtomicUsize, Ordering},
                 Arc};
+use web3::error::Error;
 use web3::{helpers::build_request, RequestId, Transport};
 
 #[derive(Clone, Debug)]
@@ -42,6 +45,24 @@ impl WebsocketTransport {
             request_id: Arc::new(AtomicUsize::new(0)),
         }
     }
+
+    async fn start_connection_loop(&self) {
+        for node in (*self.client.0.lock().await).nodes.clone() {
+            let mut wsocket = match tokio_tungstenite_wasm::connect(node.uri.to_string()).await {
+                Ok(ws) => ws,
+                Err(e) => {
+                    log::error!("{e}");
+                    continue;
+                },
+            };
+
+            // TODO
+        }
+    }
+
+    async fn stop_connection_loop(&self) { todo!() }
+
+    async fn rpc_send_and_receive(&self) { todo!() }
 }
 
 impl Transport for WebsocketTransport {
@@ -59,4 +80,13 @@ impl Transport for WebsocketTransport {
         // background task; filter the responses with the given ID and return its reponse data.
         todo!()
     }
+}
+
+async fn send_request(
+    request: Call,
+    client: Arc<WebsocketTransportRpcClient>,
+    event_handlers: Vec<RpcTransportEventHandlerShared>,
+    gui_auth_validation_generator: Option<GuiAuthValidationGenerator>,
+) -> Result<serde_json::Value, Error> {
+    todo!()
 }
