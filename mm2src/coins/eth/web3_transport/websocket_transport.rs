d336dbfa6d6f66d971b79c9fb643cc1f3f60563d
onur-ozkan <work@onurozkan.dev>
2024-02-01 17:30:29 +0300

    implement memory deallication on map pointer
    Signed-off-by: onur-ozkan <work@onurozkan.dev>

--- mm2src/coins/eth/web3_transport/websocket_transport.rs
+++ mm2src/coins/eth/web3_transport/websocket_transport.rs
@@ -14,7 +14,7 @@ use futures_ticker::Ticker;
 use futures_util::{FutureExt, SinkExt, StreamExt};
 use instant::Duration;
 use jsonrpc_core::Call;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::sync::{atomic::{AtomicUsize, Ordering},
                 Arc};
 use web3::error::Error;
@@ -41,13 +41,18 @@ pub struct WebsocketTransport {
     client: Arc<WebsocketTransportRpcClient>,
     event_handlers: Vec<RpcTransportEventHandlerShared>,
     responses: SafeMapPtr,
-    request_handler: RequestHandler,
+    controller_channel: ControllerChannel,
 }
 
 #[derive(Clone, Debug)]
-struct RequestHandler {
-    tx: UnboundedSender<WsRequest>,
-    rx: Arc<AsyncMutex<UnboundedReceiver<WsRequest>>>,
+struct ControllerChannel {
+    tx: UnboundedSender<ControllerMessage>,
+    rx: Arc<AsyncMutex<UnboundedReceiver<ControllerMessage>>>,
+}
+
+enum ControllerMessage {
+    Request(WsRequest),
+    Close,
 }
 
 #[derive(Debug)]
@@ -64,9 +69,24 @@ struct WsRequest {
 /// The implemented algorithm for socket request-response is already thread-safe,
 /// so we don't care about race conditions.
 ///
-/// TODO: gracefully deallocations
+/// As for deallocations, we use a mutex as a pointer guard in the socket connection.
+/// Whenever it is no longer held there and `drop` is called for `WebsocketTransport`,
+/// this means that the pointer is no longer in use, so we can switch raw pointer into
+/// a smart pointer (`Box`) for letting compiler to clean up the memory.
 #[derive(Clone, Debug)]
-struct SafeMapPtr(*mut HashMap<RequestId, serde_json::Value>);
+struct SafeMapPtr {
+    ptr: *mut HashMap<RequestId, serde_json::Value>,
+    guard: Arc<AsyncMutex<()>>,
+}
+
+impl Drop for WebsocketTransport {
+    fn drop(&mut self) {
+        if !self.responses.guard.try_lock().is_none() {
+            // let the compiler do the job
+            let _ = unsafe { Box::from_raw(self.responses.ptr) };
+        }
+    }
+}
 
 unsafe impl Send for SafeMapPtr {}
 unsafe impl Sync for SafeMapPtr {}
@@ -83,9 +103,12 @@ impl WebsocketTransport {
         WebsocketTransport {
             client: Arc::new(WebsocketTransportRpcClient(AsyncMutex::new(client_impl))),
             event_handlers,
-            responses: SafeMapPtr(Box::into_raw(hashmap.into())),
+            responses: SafeMapPtr {
+                ptr: Box::into_raw(hashmap.into()),
+                guard: Arc::new(AsyncMutex::new(())),
+            },
             request_id: Arc::new(AtomicUsize::new(1)),
-            request_handler: RequestHandler {
+            controller_channel: ControllerChannel {
                 tx: req_tx,
                 rx: Arc::new(AsyncMutex::new(req_rx)),
             },
@@ -93,6 +116,7 @@ impl WebsocketTransport {
     }
 
     pub async fn start_connection_loop(self) {
+        let _ptr_guard = self.responses.guard.lock().await;
         // TODO: clear disconnected channels every 30s or so.
         let mut response_map: HashMap<RequestId, oneshot::Sender<()>> = HashMap::new();
 
@@ -108,7 +132,7 @@ impl WebsocketTransport {
 
                 // id list of awaiting requests
                 let mut keepalive_interval = Ticker::new(Duration::from_secs(10));
-                let mut req_rx = self.request_handler.rx.lock().await;
+                let mut req_rx = self.controller_channel.rx.lock().await;
 
                 loop {
                     futures_util::select! {
@@ -123,15 +147,19 @@ impl WebsocketTransport {
                         }
 
                         request = req_rx.next().fuse() => {
-                            if let Some( WsRequest { request_id, request, response_notifier }) = request {
-                                let serialized_request = to_string(&request);
-                                response_map.insert(request_id, response_notifier);
-                                if let Err(e) = wsocket.send(tokio_tungstenite_wasm::Message::Text(serialized_request)).await {
-                                    log::error!("{e}");
-                                    let _ = response_map.remove(&request_id);
-                                    // TODO: try couple times at least before continue
-                                    continue;
-                                }
+                            match request {
+                                Some(ControllerMessage::Request(WsRequest { request_id, request, response_notifier })) => {
+                                    let serialized_request = to_string(&request);
+                                    response_map.insert(request_id, response_notifier);
+                                    if let Err(e) = wsocket.send(tokio_tungstenite_wasm::Message::Text(serialized_request)).await {
+                                        log::error!("{e}");
+                                        let _ = response_map.remove(&request_id);
+                                        // TODO: try couple times at least before continue
+                                        continue;
+                                    }
+                                },
+                                Some(ControllerMessage::Close) => return,
+                                _ => {},
                             }
                         }
 
@@ -147,7 +175,7 @@ impl WebsocketTransport {
                                              let request_id = id.as_u64().unwrap_or_default() as usize;
 
                                              if let Some(notifier) = response_map.remove(&request_id) {
-                                                 let response_map = unsafe { &mut *self.responses.0 };
+                                                 let response_map = unsafe { &mut *self.responses.ptr };
                                                  let _ = response_map.insert(request_id, inc_event.get("result").expect("TODO").clone());
                                                  notifier.send(()).expect("TODO");
                                              }
@@ -169,7 +197,11 @@ impl WebsocketTransport {
         }
     }
 
-    async fn stop_connection(self) { todo!() }
+    async fn stop_connection(self) {
+        let mut tx = self.controller_channel.tx.clone();
+        tx.send(ControllerMessage::Close).await.expect("TODO");
+        let _ = unsafe { Box::from_raw(self.responses.ptr) };
+    }
 }
 
 async fn send_request(
@@ -177,20 +209,20 @@ async fn send_request(
     request: Call,
     request_id: RequestId,
 ) -> Result<serde_json::Value, Error> {
-    let mut tx = transport.request_handler.tx.clone();
+    let mut tx = transport.controller_channel.tx.clone();
 
     let (notification_sender, notification_receiver) = futures::channel::oneshot::channel::<()>();
 
-    tx.send(WsRequest {
+    tx.send(ControllerMessage::Request(WsRequest {
         request_id,
         request,
         response_notifier: notification_sender,
-    })
+    }))
     .await
     .expect("TODO");
 
     if let Ok(_ping) = notification_receiver.await {
-        let response_map = unsafe { &mut *transport.responses.0 };
+        let response_map = unsafe { &mut *transport.responses.ptr };
         if let Some(response) = response_map.remove(&request_id) {
             return Ok(response);
         }
