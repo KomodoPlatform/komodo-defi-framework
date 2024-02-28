064ee806bfa6bbd26847f16bcf3d36cada1e3c1c
onur-ozkan <work@onurozkan.dev>
2024-02-29 02:07:17 +0300

    improve efficiency of known peers handling
    Signed-off-by: onur-ozkan <work@onurozkan.dev>

--- mm2src/mm2_p2p/src/behaviours/atomicdex.rs
+++ mm2src/mm2_p2p/src/behaviours/atomicdex.rs
@@ -8,12 +8,12 @@ use futures_rustls::rustls;
 use futures_ticker::Ticker;
 use instant::Duration;
 use libp2p::core::transport::Boxed as BoxedTransport;
-use libp2p::core::ConnectedPoint;
+use libp2p::core::{ConnectedPoint, Endpoint};
 use libp2p::floodsub::{Floodsub, FloodsubEvent, Topic as FloodsubTopic};
 use libp2p::gossipsub::{PublishError, SubscriptionError, ValidationMode};
 use libp2p::multiaddr::Protocol;
 use libp2p::request_response::ResponseChannel;
-use libp2p::swarm::{NetworkBehaviour, SwarmEvent, ToSwarm};
+use libp2p::swarm::{ConnectionDenied, ConnectionId, NetworkBehaviour, SwarmEvent, ToSwarm};
 use libp2p::{identity, noise, PeerId, Swarm};
 use libp2p::{Multiaddr, Transport};
 use log::{debug, error, info};
@@ -736,19 +736,7 @@ fn start_gossipsub(
     let polling_fut = poll_fn(move |cx: &mut Context| {
         loop {
             match swarm.behaviour_mut().cmd_rx.poll_next_unpin(cx) {
-                Poll::Ready(Some(cmd)) => {
-                    if let AdexBehaviourCmd::AddReservedPeer { peer, addresses } = &cmd {
-                        if !swarm.is_connected(peer) {
-                            for address in addresses {
-                                if let Err(e) = swarm.dial(address.clone()) {
-                                    error!("Dialing failed on '{peer}'. Error: {e}");
-                                }
-                            }
-                        }
-                    };
-
-                    swarm.behaviour_mut().process_cmd(cmd).unwrap()
-                },
+                Poll::Ready(Some(cmd)) => swarm.behaviour_mut().process_cmd(cmd).unwrap(),
                 Poll::Ready(None) => return Poll::Ready(()),
                 Poll::Pending => break,
             }
@@ -1004,6 +992,51 @@ impl NetworkBehaviour for AtomicDexBehaviour {
     ) -> Result<libp2p::swarm::THandler<Self>, libp2p::swarm::ConnectionDenied> {
         self.core
             .handle_established_outbound_connection(connection_id, peer, addr, role_override)
+    }
+
+    fn handle_pending_outbound_connection(
+        &mut self,
+        connection_id: ConnectionId,
+        maybe_peer: Option<PeerId>,
+        addresses: &[Multiaddr],
+        effective_role: Endpoint,
+    ) -> Result<Vec<Multiaddr>, ConnectionDenied> {
+        let mut found_addresses = self.core.request_response.handle_pending_outbound_connection(
+            connection_id,
+            maybe_peer,
+            addresses,
+            effective_role,
+        )?;
+
+        found_addresses.extend(self.core.peers_exchange.handle_pending_outbound_connection(
+            connection_id,
+            maybe_peer,
+            addresses,
+            effective_role,
+        )?);
+
+        found_addresses.extend(self.core.gossipsub.handle_pending_outbound_connection(
+            connection_id,
+            maybe_peer,
+            addresses,
+            effective_role,
+        )?);
+
+        found_addresses.extend(self.core.floodsub.handle_pending_outbound_connection(
+            connection_id,
+            maybe_peer,
+            addresses,
+            effective_role,
+        )?);
+
+        found_addresses.extend(self.core.ping.handle_pending_outbound_connection(
+            connection_id,
+            maybe_peer,
+            addresses,
+            effective_role,
+        )?);
+
+        Ok(found_addresses)
     }
 
     fn on_swarm_event(&mut self, event: libp2p::swarm::FromSwarm<Self::ConnectionHandler>) {
