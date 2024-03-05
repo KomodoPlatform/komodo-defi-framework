0476609eaa91ab9be8de9c85fb7248641a117432
shamardy <shamardy@yahoo.com>
2024-03-05 23:47:56 +0200

    fix wasm lint

--- mm2src/mm2_net/src/wasm_event_stream.rs
+++ mm2src/mm2_net/src/wasm_event_stream.rs
@@ -21,7 +21,7 @@ pub async fn handle_worker_stream(ctx: MmArc) {
             .worker_path
             .to_str()
             .expect("worker_path contains invalid UTF-8 characters");
-        let worker = web_sys::Worker::new(worker_path).expect(&format!("Missing {}", worker_path));
+        let worker = web_sys::Worker::new(worker_path).unwrap_or_else(|_| panic!("Missing {}", worker_path));
         let message_js = wasm_bindgen::JsValue::from_str(&data.to_string());
 
         worker.post_message(&message_js)
