812efcd154c18b749dc0b22ecfeb6f029771fb87
onur-ozkan <work@onurozkan.dev>
2024-04-05 15:34:34 +0300

    add TODO
    Signed-off-by: onur-ozkan <work@onurozkan.dev>

--- mm2src/coins_activation/src/tendermint_with_assets_activation.rs
+++ mm2src/coins_activation/src/tendermint_with_assets_activation.rs
@@ -220,6 +220,10 @@ impl PlatformWithTokensActivationOps for TendermintCoin {
         activation_request: Self::ActivationRequest,
         protocol_conf: Self::PlatformProtocolInfo,
     ) -> Result<Self, MmError<Self::ActivationError>> {
+        // TODO: fail if:
+        // - mm2 is on watcher mode
+        // - mm2 using watchers
+
         let conf = TendermintConf::try_from_json(&ticker, coin_conf)?;
 
         let activation_policy = if let Some(pubkey) = activation_request.with_pubkey {
