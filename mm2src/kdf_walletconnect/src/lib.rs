39579351111781523d3217acccf04307852b7d61
Samuel Onoja <samiodev@icloud.com>
2024-08-08 14:49:18 +0100

    rename mod and add port info

--- mm2src/kdf_walletconnect/src/lib.rs
+++ mm2src/kdf_walletconnect/src/lib.rs
@@ -1,3 +1,14 @@
+//! # WalletConnect WebSocket Client
+//!
+//! The WalletConnect client implementation has undergone enhancements to support both WASM and non-WASM targets.
+//! Initially, the implementation was restricted to non-WASM targets,
+//! which limited its utility in web-based environments.
+//!
+//! This module contains the WebSocket client code that has been ported and modified to achieve cross-platform compatibility.
+//! The changes ensure that the WalletConnect client can function seamlessly in both native
+//! and WASM targets.
+//!
+
 #[allow(unused)] mod client;
 mod error;
 extern crate common;
