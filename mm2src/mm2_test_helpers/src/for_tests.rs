a69f3153c3b41729cfcf79d169e4d85f28e10dca
shamardy <shamardy@yahoo.com>
2024-10-10 13:29:54 +0300

    remove enable_spl fn in tests

--- mm2src/mm2_test_helpers/src/for_tests.rs
+++ mm2src/mm2_test_helpers/src/for_tests.rs
@@ -2029,21 +2029,6 @@ pub async fn enable_eth_coin(
     json::from_str(&enable.1).unwrap()
 }
 
-pub async fn enable_spl(mm: &MarketMakerIt, coin: &str) -> Json {
-    let req = json!({
-        "userpass": mm.userpass,
-        "method": "enable_spl",
-        "mmrpc": "2.0",
-        "params": {
-            "ticker": coin,
-            "activation_params": {}
-        }
-    });
-    let enable = mm.rpc(&req).await.unwrap();
-    assert_eq!(enable.0, StatusCode::OK, "'enable_spl' failed: {}", enable.1);
-    json::from_str(&enable.1).unwrap()
-}
-
 pub async fn enable_slp(mm: &MarketMakerIt, coin: &str) -> Json {
     let enable = mm
         .rpc(&json!({
