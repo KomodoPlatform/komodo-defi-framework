a028e456ddc257fb89eaea146e8e5635f7df5ccc
onur-ozkan <work@onurozkan.dev>
2024-10-30 11:32:41 +0300

    add doc-comments
    Signed-off-by: onur-ozkan <work@onurozkan.dev>

--- mm2src/mm2_net/src/p2p/request_response/mod.rs
+++ mm2src/mm2_net/src/p2p/request_response/mod.rs
@@ -1,12 +1,17 @@
-//! Types to use with p2p request-response protocol.
-
-use serde::{Deserialize, Serialize};
+//! This module defines types exclusively for the request-response P2P protocol
+//! which are separate from other request types such as RPC requests or Gossipsub
+//! messages.
 
 pub mod network_info;
 pub mod ordermatch;
 
+use serde::{Deserialize, Serialize};
+
+/// Wrapper type for handling request-response P2P requests.
 #[derive(Eq, Debug, Deserialize, PartialEq, Serialize)]
 pub enum P2PRequest {
+    /// Request for order matching.
     Ordermatch(ordermatch::OrdermatchRequest),
+    /// Request for network information from the target peer.
     NetworkInfo(network_info::NetworkInfoRequest),
 }
