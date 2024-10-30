5f2eae5468ebfdd0525f7db526a822c200232ebd
onur-ozkan <work@onurozkan.dev>
2024-10-30 12:05:46 +0300

    remove leftovers
    Signed-off-by: onur-ozkan <work@onurozkan.dev>

--- mm2src/mm2_main/src/lp_network.rs
+++ mm2src/mm2_main/src/lp_network.rs
@@ -227,9 +227,7 @@ fn process_p2p_request(
     request: Vec<u8>,
     response_channel: mm2_libp2p::AdexResponseChannel,
 ) -> P2PRequestResult<()> {
-    // TODO: explanation
     let request = decode_message::<P2PRequest>(&request)?;
-
     let result = match request {
         P2PRequest::Ordermatch(req) => lp_ordermatch::process_peer_request(ctx.clone(), req),
         P2PRequest::NetworkInfo(req) => lp_stats::process_info_request(ctx.clone(), req),
