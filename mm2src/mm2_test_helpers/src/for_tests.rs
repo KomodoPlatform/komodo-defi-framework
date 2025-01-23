55b3f9392499e144c08c3395e272af5a6358422b
Alright <mzlot555@gmail.com>
2025-01-23 10:58:59 -0500

    cargo fmt

--- mm2src/mm2_test_helpers/src/for_tests.rs
+++ mm2src/mm2_test_helpers/src/for_tests.rs
@@ -241,7 +241,11 @@ pub const ETH_MAINNET_NODE: &str = "https://mainnet.infura.io/v3/c01c1b4cf666425
 pub const ETH_MAINNET_CHAIN_ID: u64 = 1;
 pub const ETH_MAINNET_SWAP_CONTRACT: &str = "0x24abe4c71fc658c91313b6552cd40cd808b3ea80";
 
-pub const ETH_SEPOLIA_NODES: &[&str] = &["https://ethereum-sepolia-rpc.publicnode.com","https://rpc2.sepolia.org","https://1rpc.io/sepolia"];
+pub const ETH_SEPOLIA_NODES: &[&str] = &[
+    "https://ethereum-sepolia-rpc.publicnode.com",
+    "https://rpc2.sepolia.org",
+    "https://1rpc.io/sepolia",
+];
 pub const ETH_SEPOLIA_CHAIN_ID: u64 = 11155111;
 pub const ETH_SEPOLIA_SWAP_CONTRACT: &str = "0xeA6D65434A15377081495a9E7C5893543E7c32cB";
 pub const ETH_SEPOLIA_TOKEN_CONTRACT: &str = "0x09d0d71FBC00D7CCF9CFf132f5E6825C88293F19";
@@ -1435,8 +1439,7 @@ impl MarketMakerIt {
         }
 
         let ctx = {
-            let builder = MmCtxBuilder::new()
-                .with_conf(conf.clone());
+            let builder = MmCtxBuilder::new().with_conf(conf.clone());
 
             let builder = if let Some(ns) = db_namespace_id {
                 builder.with_test_db_namespace_with_id(ns)
@@ -3268,18 +3271,19 @@ async fn init_erc20_token(
     protocol: Option<Json>,
     path_to_address: Option<HDAccountAddressId>,
 ) -> Result<(StatusCode, Json), Json> {
-    let (status, response, _) = mm.rpc(&json!({
-        "userpass": mm.userpass,
-        "method": "task::enable_erc20::init",
-        "mmrpc": "2.0",
-        "params": {
-            "ticker": ticker,
-            "protocol": protocol,
-            "activation_params": {
-                "path_to_address": path_to_address.unwrap_or_default(),
+    let (status, response, _) = mm
+        .rpc(&json!({
+            "userpass": mm.userpass,
+            "method": "task::enable_erc20::init",
+            "mmrpc": "2.0",
+            "params": {
+                "ticker": ticker,
+                "protocol": protocol,
+                "activation_params": {
+                    "path_to_address": path_to_address.unwrap_or_default(),
+                }
             }
-        }
-    }))
+        }))
         .await
         .unwrap();
 
@@ -3349,12 +3353,7 @@ pub async fn get_token_info(mm: &MarketMakerIt, protocol: Json) -> TokenInfoResp
         }))
         .await
         .unwrap();
-    assert_eq!(
-        response.0,
-        StatusCode::OK,
-        "'get_token_info' failed: {}",
-        response.1
-    );
+    assert_eq!(response.0, StatusCode::OK, "'get_token_info' failed: {}", response.1);
     let response_json: Json = json::from_str(&response.1).unwrap();
     json::from_value(response_json["result"].clone()).unwrap()
 }
