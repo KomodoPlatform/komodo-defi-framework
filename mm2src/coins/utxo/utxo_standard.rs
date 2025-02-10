4d9495a8ddf6eb45baafba587dd406c9dd3e53ce
Alright <mzlot555@gmail.com>
2025-02-10 11:19:07 -0500

    use default impl of SwapOps::is_auto_refundable when applicable

--- mm2src/coins/utxo/utxo_standard.rs
+++ mm2src/coins/utxo/utxo_standard.rs
@@ -446,8 +446,6 @@ impl SwapOps for UtxoStandardCoin {
             .map_err(|e| ERRL!("{}", e))
     }
 
-    fn is_auto_refundable(&self) -> bool { false }
-
     async fn wait_for_htlc_refund(&self, _tx: &[u8], _locktime: u64) -> RefundResult<()> {
         MmError::err(RefundError::Internal(
             "wait_for_htlc_refund is not supported for this coin!".into(),
