9cea6e34d6b24eda8ee3556ce09614f8c1081aeb
shamardy <shamardy@yahoo.com>
2025-02-19 17:58:46 +0200

    remove comment from `test_request_response_ok_three_peers`

--- mm2src/mm2_p2p/src/behaviours/mod.rs
+++ mm2src/mm2_p2p/src/behaviours/mod.rs
@@ -144,11 +144,6 @@ mod tests {
         assert!(request_received.load(Ordering::Relaxed));
     }
 
-    /// Failed on macOS with the below error:
-    /// ```text
-    /// ---- behaviours::tests::test_request_response_ok_three_peers stdout ----
-    /// thread 'behaviours::tests::test_request_response_ok_three_peers' panicked at 'called `Option::unwrap()` on a `None` value', mm2src/mm2_p2p/src/behaviours/mod.rs:236:58
-    /// ```
     #[tokio::test]
     #[cfg(target_os = "linux")] // https://github.com/KomodoPlatform/atomicDEX-API/issues/1712
     async fn test_request_response_ok_three_peers() {
