afa5bc16609303c9486ce4a8eb0ed44fd951cd3d
Samuel Onoja <samiodev@icloud.com>
2025-03-21 09:26:24 +0100

    log signed_tx info

--- mm2src/coins/eth/wallet_connect.rs
+++ mm2src/coins/eth/wallet_connect.rs
@@ -116,6 +116,7 @@ impl WalletConnectOps for EthCoin {
             hex::decode(tx_hex)?
         };
 
+        info!("signed_tx bytes: {bytes:?}");
         let unverified = rlp::decode(&bytes)?;
         let signed = SignedTransaction::new(unverified)?;
         let bytes = rlp::encode(&signed);
@@ -240,7 +241,7 @@ pub(crate) async fn send_transaction_with_walletconnect(
     data: &[u8],
     gas: U256,
 ) -> Result<SignedTransaction, TransactionErr> {
-    info!(target: "WalletConnect: sign-and-send", "get_gas_price…");
+    info!("target: WalletConnect: sign-and-send, get_gas_price…");
     let pay_for_gas_option = try_tx_s!(
         coin.get_swap_pay_for_gas_option(coin.get_swap_transaction_fee_policy())
             .await
@@ -258,7 +259,7 @@ pub(crate) async fn send_transaction_with_walletconnect(
     };
     // Please note that this method may take a long time
     // due to `eth_sendTransaction` requests.
-    info!(target: "WalletConnect: sign-and-send", "signing and sending tx…");
+    info!("target: WalletConnect: sign-and-send, signing and sending tx");
     let (signed_tx, _) = try_tx_s!(coin.wc_send_tx(wc, params).await);
 
     Ok(signed_tx)
