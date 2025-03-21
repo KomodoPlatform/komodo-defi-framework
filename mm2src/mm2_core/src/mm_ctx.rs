88475e5eb2e68af9a6d993038db766ffc7da72c8
shamardy <shamardy@yahoo.com>
2025-03-21 20:31:23 +0200

    fix: allow spaces in wallet name

--- mm2src/mm2_core/src/mm_ctx.rs
+++ mm2src/mm2_core/src/mm_ctx.rs
@@ -301,16 +301,24 @@ impl MmCtx {
     pub fn db_root(&self) -> PathBuf { path_to_db_root(self.conf["dbdir"].as_str()) }
     #[cfg(not(target_arch = "wasm32"))]
     pub fn wallet_file_path(&self, wallet_name: &str) -> Result<PathBuf, String> {
-        if !wallet_name.chars().all(|c| c.is_alphanumeric() || c == '-' || c == '_') {
+        let wallet_name_trimmed = wallet_name.trim();
+        if wallet_name_trimmed.is_empty() {
+            return Err("Wallet name cannot be empty or consist only of whitespace.".to_string());
+        }
+
+        if !wallet_name_trimmed
+            .chars()
+            .all(|c| c.is_alphanumeric() || c == '-' || c == '_' || c == ' ')
+        {
             return Err(format!(
-                "Invalid wallet name: '{}'. Only alphanumeric characters, dash and underscore are allowed.",
-                wallet_name
+                "Invalid wallet name: '{}'. Only alphanumeric characters, spaces, dash and underscore are allowed.",
+                wallet_name_trimmed
             ));
         }
 
         Ok(self
             .db_root()
-            .join(format!("{}.{}", wallet_name, WALLET_FILE_EXTENSION)))
+            .join(format!("{}.{}", wallet_name_trimmed, WALLET_FILE_EXTENSION)))
     }
 
     /// MM database path.  
