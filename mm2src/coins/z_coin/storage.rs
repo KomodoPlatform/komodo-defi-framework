78137d3f78b20631181c3acbfc10588cab13e15b
Alright <mzlot555@gmail.com>
2025-04-04 17:04:21 -0400

    remove unused import

--- mm2src/coins/z_coin/storage.rs
+++ mm2src/coins/z_coin/storage.rs
@@ -17,7 +17,6 @@ use mm2_err_handle::mm_error::MmResult;
 #[cfg(target_arch = "wasm32")]
 use walletdb::wasm::storage::DataConnStmtCacheWasm;
 #[cfg(debug_assertions)]
-use zcash_client_backend::data_api::error::Error;
 use zcash_client_backend::data_api::PrunedBlock;
 use zcash_client_backend::proto::compact_formats::CompactBlock;
 use zcash_client_backend::wallet::{AccountId, WalletTx};
