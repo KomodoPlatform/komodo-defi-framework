4b0f8c0b7f9039b1e4cfdb9cf555b5c5c7645421
onur-ozkan <work@onurozkan.dev>
2025-04-04 13:56:47 +0300

    fmt fix
    Signed-off-by: onur-ozkan <work@onurozkan.dev>

--- mm2src/mm2_net/src/lib.rs
+++ mm2src/mm2_net/src/lib.rs
@@ -1,7 +1,7 @@
 pub mod event_streaming;
 pub mod grpc_web;
-pub mod transport;
 pub mod ip_addr;
+pub mod transport;
 
 #[cfg(not(target_arch = "wasm32"))] pub mod native_http;
 #[cfg(not(target_arch = "wasm32"))] pub mod native_tls;
