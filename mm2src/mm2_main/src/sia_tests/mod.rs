f28494d2c7ecb332c31e841fc694401fab855fbc
Alright <mzlot555@gmail.com>
2025-04-17 09:06:47 -0400

    temporarily remove sia_tests feature gates to test current CI Actions

--- mm2src/mm2_main/src/sia_tests/mod.rs
+++ mm2src/mm2_main/src/sia_tests/mod.rs
@@ -1,9 +1,9 @@
-#[cfg(feature = "run-sia-functional-tests")]
+// #[cfg(feature = "run-sia-functional-tests")]
 mod docker_functional_tests;
 
 /// This module is a temporary hack to allow grouping the relevant tests together via `cargo test` commands.
 /// See doc comment inside short_locktime_tests.rs for more details.
-#[cfg(feature = "run-sia-functional-tests-short-locktime")]
+// #[cfg(feature = "run-sia-functional-tests-short-locktime")]
 mod short_locktime_tests;
 
 pub(crate) mod utils;
