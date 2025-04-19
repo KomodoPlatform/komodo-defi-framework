d15ddfccb55fe0d463276ed5d19064323329ab27
Alright <mzlot555@gmail.com>
2025-04-19 19:40:18 -0400

    add doc comments regarding sia tests modules

--- mm2src/mm2_main/src/sia_tests/mod.rs
+++ mm2src/mm2_main/src/sia_tests/mod.rs
@@ -1,9 +1,12 @@
-// #[cfg(feature = "run-sia-functional-tests")]
+/// These modules are feature gated behind "run-sia-functional-tests" feature.
+/// Each module must be run individually.
+/// Eg,
+/// cargo test -p mm2_main --features enable-sia,run-sia-functional-tests -- sia_tests::docker_functional_tests
+/// cargo test -p mm2_main --features enable-sia,run-sia-functional-tests -- sia_tests::short_locktime_tests
 mod docker_functional_tests;
 
 /// This module is a temporary hack to allow grouping the relevant tests together via `cargo test` commands.
 /// See doc comment inside short_locktime_tests.rs for more details.
-// #[cfg(feature = "run-sia-functional-tests-short-locktime")]
 mod short_locktime_tests;
 
 pub(crate) mod utils;
