8a42b0ee89bca55ccabf2d0729eccd31ef8fda14
Samuel Onoja <samiodev@icloud.com>
2025-04-30 03:02:17 +0100

    minor changes to eth test

--- mm2src/mm2_main/tests/mm2_tests/mm2_tests_inner.rs
+++ mm2src/mm2_main/tests/mm2_tests/mm2_tests_inner.rs
@@ -5087,6 +5087,70 @@ fn test_sign_verify_message_utxo_segwit() {
 
 #[test]
 #[cfg(not(target_arch = "wasm32"))]
+fn test_sign_verify_message_eth() {
+    let seed = "spice describe gravity federal blast come thank unfair canal monkey style afraid";
+
+    let coins = json!([
+        {
+            "coin": "ETH",
+            "name": "ethereum",
+            "fname": "Ethereum",
+            "sign_message_prefix": "Ethereum Signed Message:\n",
+            "rpcport": 80,
+            "mm2": 1,
+            "chain_id": 1,
+            "required_confirmations": 3,
+            "avg_blocktime": 0.25,
+            "protocol": {"type": "ETH"}
+        }
+    ]);
+
+    // start bob and immediately place the order
+    let mm_bob = MarketMakerIt::start(
+        json! ({
+            "gui": "nogui",
+            "netid": 9998,
+            "myipaddr": env::var ("BOB_TRADE_IP") .ok(),
+            "rpcip": env::var ("BOB_TRADE_IP") .ok(),
+            "canbind": env::var ("BOB_TRADE_PORT") .ok().map (|s| s.parse::<i64>().unwrap()),
+            "passphrase": seed.to_string(),
+            "coins": coins,
+            "rpc_password": "pass",
+            "i_am_seed": true,
+        }),
+        "pass".into(),
+        None,
+    )
+    .unwrap();
+    let (_bob_dump_log, _bob_dump_dashboard) = mm_bob.mm_dump();
+    log!("Bob log path: {}", mm_bob.log_path.display());
+
+    // Enable coins on Bob side. Print the replies in case we need the "address".
+    log!(
+        "enable_coins (bob): {:?}",
+        block_on(enable_native(&mm_bob, "ETH", ETH_SEPOLIA_NODES, None))
+    );
+
+    let response = block_on(sign_message(&mm_bob, "ETH", None));
+    let response: RpcV2Response<SignatureResponse> = json::from_value(response).unwrap();
+    let response = response.result;
+
+    assert_eq!(
+        response.signature,
+        "0xcdf11a9c4591fb7334daa4b21494a2590d3f7de41c7d2b333a5b61ca59da9b311b492374cc0ba4fbae53933260fa4b1c18f15d95b694629a7b0620eec77a938600"
+    );
+
+    let response = block_on(verify_message(&mm_bob, "ETH",
+                                           "0xcdf11a9c4591fb7334daa4b21494a2590d3f7de41c7d2b333a5b61ca59da9b311b492374cc0ba4fbae53933260fa4b1c18f15d95b694629a7b0620eec77a938600",
+                                           "0xbAB36286672fbdc7B250804bf6D14Be0dF69fa29"));
+    let response: RpcV2Response<VerificationResponse> = json::from_value(response).unwrap();
+    let response = response.result;
+
+    assert!(response.is_valid);
+}
+
+#[test]
+#[cfg(not(target_arch = "wasm32"))]
 fn test_sign_verify_message_eth_with_derivation_path() {
     use crypto::DerivationPath;
 
@@ -5150,8 +5214,9 @@ fn test_sign_verify_message_eth_with_derivation_path() {
         "'enable_eth_with_tokens' failed: {}",
         enable.1
     );
-    let body: Json = json::from_str(&enable.1).unwrap();
-    log!("enable_coins (bob): {body:?}");
+    let result: Json = json::from_str(&enable.1).unwrap();
+    let result: HDEthWithTokensActivationResult = json::from_value(result["result"].clone()).unwrap();
+    log!("enable_coins (bob): {result:?}");
 
     let response = block_on(sign_message(
         &mm_bob,
@@ -5165,18 +5230,17 @@ fn test_sign_verify_message_eth_with_derivation_path() {
         "0x36b91a54f905f2dd88ecfd7f4a539710c699eaab2b425ba79ad959c29ec26492011674981da72d68ac0ab72bb35661a13c42bce314ecdfff0e44174f82a7ee2501";
     assert_eq!(expected_signature, response.signature);
 
-    let response = block_on(verify_message(
-        &mm_bob,
-        "ETH",
-        expected_signature,
-        "0x1737F1FaB40c6Fd3dc729B51C0F97DB3297CCA93",
-    ));
+    let address0 = match result.wallet_balance {
+        EnableCoinBalanceMap::HD(bal) => bal.accounts[0].addresses[0].address.clone(),
+        EnableCoinBalanceMap::Iguana(_) => panic!("Expected HD"),
+    };
+    let response = block_on(verify_message(&mm_bob, "ETH", expected_signature, &address0));
     let response: RpcV2Response<VerificationResponse> = json::from_value(response).unwrap();
     let response = response.result;
 
     assert!(response.is_valid);
 
-    // Test another address.
+    // Test address 1.
     let get_new_address = block_on(get_new_address(&mm_bob, "ETH", 0, Some(Bip44Chain::External)));
     assert!(get_new_address.new_address.balance.contains_key("ETH"));
     let response = block_on(sign_message(
@@ -5197,70 +5261,6 @@ fn test_sign_verify_message_eth_with_derivation_path() {
         expected_signature,
         &get_new_address.new_address.address,
     ));
-    let response: RpcV2Response<VerificationResponse> = json::from_value(response).unwrap();
-    let response = response.result;
-
-    assert!(response.is_valid);
-}
-
-#[test]
-#[cfg(not(target_arch = "wasm32"))]
-fn test_sign_verify_message_eth() {
-    let seed = "spice describe gravity federal blast come thank unfair canal monkey style afraid";
-
-    let coins = json!([
-        {
-            "coin": "ETH",
-            "name": "ethereum",
-            "fname": "Ethereum",
-            "sign_message_prefix": "Ethereum Signed Message:\n",
-            "rpcport": 80,
-            "mm2": 1,
-            "chain_id": 1,
-            "required_confirmations": 3,
-            "avg_blocktime": 0.25,
-            "protocol": {"type": "ETH"}
-        }
-    ]);
-
-    // start bob and immediately place the order
-    let mm_bob = MarketMakerIt::start(
-        json! ({
-            "gui": "nogui",
-            "netid": 9998,
-            "myipaddr": env::var ("BOB_TRADE_IP") .ok(),
-            "rpcip": env::var ("BOB_TRADE_IP") .ok(),
-            "canbind": env::var ("BOB_TRADE_PORT") .ok().map (|s| s.parse::<i64>().unwrap()),
-            "passphrase": seed.to_string(),
-            "coins": coins,
-            "rpc_password": "pass",
-            "i_am_seed": true,
-        }),
-        "pass".into(),
-        None,
-    )
-    .unwrap();
-    let (_bob_dump_log, _bob_dump_dashboard) = mm_bob.mm_dump();
-    log!("Bob log path: {}", mm_bob.log_path.display());
-
-    // Enable coins on Bob side. Print the replies in case we need the "address".
-    log!(
-        "enable_coins (bob): {:?}",
-        block_on(enable_native(&mm_bob, "ETH", ETH_SEPOLIA_NODES, None))
-    );
-
-    let response = block_on(sign_message(&mm_bob, "ETH", None));
-    let response: RpcV2Response<SignatureResponse> = json::from_value(response).unwrap();
-    let response = response.result;
-
-    assert_eq!(
-        response.signature,
-        "0xcdf11a9c4591fb7334daa4b21494a2590d3f7de41c7d2b333a5b61ca59da9b311b492374cc0ba4fbae53933260fa4b1c18f15d95b694629a7b0620eec77a938600"
-    );
-
-    let response = block_on(verify_message(&mm_bob, "ETH",
-                                           "0xcdf11a9c4591fb7334daa4b21494a2590d3f7de41c7d2b333a5b61ca59da9b311b492374cc0ba4fbae53933260fa4b1c18f15d95b694629a7b0620eec77a938600",
-                                           "0xbAB36286672fbdc7B250804bf6D14Be0dF69fa29"));
     let response: RpcV2Response<VerificationResponse> = json::from_value(response).unwrap();
     let response = response.result;
 
