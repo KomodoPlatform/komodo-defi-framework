824576741a2361da6717b9fa229565f1a65d060f
Onur Özkan <work@onurozkan.dev>
2025-07-07 15:07:10 +0300

    remove auto trait in state_machine
    Signed-off-by: Onur Özkan <work@onurozkan.dev>

--- mm2src/mm2_state_machine/src/lib.rs
+++ mm2src/mm2_state_machine/src/lib.rs
@@ -1,9 +1,9 @@
-#![feature(auto_traits, negative_impls)]
+#![feature(negative_impls)]
 
 pub mod prelude;
 pub mod state_machine;
 pub mod storable_state_machine;
 
 
-pub auto trait NotSame {}
+pub trait NotSame {}
 impl<X> !NotSame for (X, X) {}
