66ec22ab78fe6ef6aa11ec5f86c32bc636aac2b1
Onur Özkan <work@onurozkan.dev>
2025-07-07 15:06:31 +0300

    remove redundant trait implementation
    Signed-off-by: Onur Özkan <work@onurozkan.dev>

--- mm2src/mm2_state_machine/src/lib.rs
+++ mm2src/mm2_state_machine/src/lib.rs
@@ -1,12 +1,9 @@
-#![feature(allocator_api, auto_traits, negative_impls)]
+#![feature(auto_traits, negative_impls)]
 
 pub mod prelude;
 pub mod state_machine;
 pub mod storable_state_machine;
 
-use std::alloc::Allocator;
 
 pub auto trait NotSame {}
 impl<X> !NotSame for (X, X) {}
-// Makes the error conversion work for structs/enums containing Box<dyn ...>
-impl<T: ?Sized, A: Allocator> NotSame for Box<T, A> {}
